1954bf8045f7a881f26afb931f5fef5f8dd05f21
Pradeep Shukla <38289787+shuklas101@users.noreply.github.com>
2020-09-18 10:43:01 -0500

    final code commit

---
+++ CARuleAnalysis/Helper.vb
@@ -0,0 +1,12 @@
+﻿Public Class Helper
+
+    Public Sub HandleDropdown()
+        MessageBox.Show("test")
+    End Sub
+
+
+    Public Sub GeneratePreApprovalOutputDocument()
+        MessageBox.Show("test2")
+    End Sub
+
+End Class
